d851898c4e61409a47a94a815ace5e2f3ffbb4dc
Opara Bright <oparauchenna57@gmail.com>
2026-06-17 00:13:33 +0100

    Fix env variables for PHP server

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
-if [ ! -f config.php ]; then
-  cp config.sample.php config.php
-fi
-
 : "${PORT:=8000}"
+
+export DB_HOST="${DB_HOST:-127.0.0.1}"
+export DB_NAME="${DB_NAME:-school_event_access}"
+export DB_USER="${DB_USER:-root}"
+export DB_PASS="${DB_PASS:-}"
+export DB_PORT="${DB_PORT:-3306}"
+export SCHOOL_NAME="${SCHOOL_NAME:-School Event Check-In}"
+export STAFF_USERNAME="${STAFF_USERNAME:-admin}"
+export STAFF_PASSWORD="${STAFF_PASSWORD:-change-this-password}"
+
 php -S 0.0.0.0:$PORT -t .
